40f1a2584442e393dfbcbc42e79609863b9f143f
Sergey Karinskiy <kblp@yandex.ru>
2015-04-10 20:05:10 +0700

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # u.js
-Microframework for easy manipulation with variables
+Simplifies the handling of data types and allows to create custom types with handlers
+
+# Installation
+### Using npm:
+```bash
+$ npm install u.js
+```
+
+### Using bower:
+```bash
+$ bower install u.js
+```
+
+### Download:
+[u.js](https://raw.githubusercontent.com/desker/u.js/master/dist/u.js), *{{libSize}}* kb
+[u.min.js](https://raw.githubusercontent.com/desker/u.js/master/dist/u.min.js), *{{libSizePacked}}* kb
---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp'),
+    $ = require('gulp-load-plugins')(),
+    bsync = require('browser-sync'),
+    reload = bsync.reload;
+
+gulp.task('js:lint', function() {
+  return gulp.src('./src/*.js')
+    .pipe($.plumber({errorHandler: $.notify("Error js:lint")}))
+    .pipe($.jshint())
+    .pipe($.notify(function (file) {
+      if (file.jshint.success) return false;
+ 
+      var errors = file.jshint.results.map(function (data) {
+        if (data.error) {
+          return "(" + data.error.line + ':' + data.error.character + ') ' + data.error.reason;
+        }
+      }).join("\n");
+      return file.relative + " (" + file.jshint.results.length + " errors)\n" + errors;
+    }));
+});
+
+gulp.task('js:concat', function() {
+  return gulp.src('./src/*.js')
+    .pipe($.plumber({errorHandler: $.notify("Error js:concat")}))  
+    .pipe($.concat('u.js'))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('js:min', ['js:concat'], function() {
+  return gulp.src('./dist/u.js')
+    .pipe($.plumber({errorHandler: $.notify("Error js:min")}))
+    .pipe($.uglify())
+    .pipe($.rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./dist'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('js', ['js:lint', 'js:concat', 'js:min'])
+
+gulp.task('default', ['js'], function() {
+  bsync({
+    server: {
+      baseDir: ['./dist', './examples/']
+    }
+  });
+
+  gulp.watch('./src/**/*.js', ['js']);
+});
